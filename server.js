c9bdbb728b091360892eee8d260eea9fcd5e47e6
System <none>
2017-09-18 20:32:21 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express     = require('express'),
     ejs         = require('ejs'),
     mongoose    = require('mongoose'),
-    bodyParser  = require('body-parser');
+    bodyParser  = require('body-parser'),
+    shortid     = require('shortid');
 
 var app = express();
 
@@ -9,8 +10,7 @@ mongoose.connect('mongodb://lincoln:password@ds062448.mlab.com:62448/lincbarturl
 
 var urlSchema = new mongoose.Schema({
     destination: String,
-    path : String
-  
+    id : String 
 });
 
 var shortUrl = mongoose.model('Short', urlSchema);
@@ -24,17 +24,22 @@ app.get("/", function (req, res) {
   res.render('index');
 });
 
-app.post("/new/:id", function (req, res) {
+app.get("/new/:url", function (req, res) {
+  var newId = shortid.generate();
+  console.log(newId);
   var newShort = {
-    destination: req.params.id,
-    path:
+    destination: req.params.url,
+    id: newId
   }
-  shortUrl.create()
+  shortUrl.create(newShort, function(err,newShort){
+      res.send(newShort);  
+  });
   
+  console.log(newShort);
 });
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-});
+}); 
